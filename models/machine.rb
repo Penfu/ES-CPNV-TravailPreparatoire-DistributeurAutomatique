d0c94b1ed63cae58a203eb1daf6cc69258a14f2f
Penfu <sypenfu@gmail.com>
2022-04-08 22:06:17 +0200

    Implement Sale in Machine system

--- models/machine.rb
+++ models/machine.rb
@@ -1,4 +1,6 @@
 class Machine
+    require 'date'
+    
     require_relative '../exceptions/item_not_found'
     require_relative '../exceptions/item_out_of_stock'
     require_relative '../exceptions/not_enough_money'
@@ -7,6 +9,7 @@ class Machine
         @amount = 0
         @balance = 0
         @items = items
+        @sales = []
     end
 
     def insert(amount)
@@ -20,6 +23,7 @@ class Machine
         raise ItemOutOfStock.new "Item #{item.name}: Out ofstock!"     if item.quantity == 0
         raise NotEnoughMoney.new "Not enough money!"                   if item.price > @amount
         
+        @sales << Sale.new(item, !@datetime ? Time.now : @datetime)
         @amount -= item.price
         @balance += item.price
         item.quantity -= 1
@@ -33,5 +37,16 @@ class Machine
 
     def get_balance
         @balance.round(2)
+    end
+
+    def best_sales
+        @sales
+        .group_by { |sale| sale.datetime.hour }
+        .map {|hour, sales| [hour, sales.sum { |sale| sale.item.price }] }
+        .sort_by { |hour, sales| sales }.reverse.take(3)
+    end
+
+    def set_time(datetime)
+        @datetime = datetime
     end
 end
