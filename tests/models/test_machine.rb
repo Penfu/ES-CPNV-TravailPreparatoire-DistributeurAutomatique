089d609f9c4d959d6284a6a26cdfd876ec637ee7
Penfu <sypenfu@gmail.com>
2022-04-08 22:04:53 +0200

    Add Sale tests

--- tests/models/test_machine.rb
+++ tests/models/test_machine.rb
@@ -17,97 +17,97 @@ class TestMachine < MiniTest::Test
     ])
   end
 
-    def test_choose_an_item_without_money_raises_exception
-      item = "A01"
+  def test_choose_an_item_without_money_raises_exception
+    item = "A01"
 
-      assert_raises NotEnoughMoney do @machine.choose(item) end
-      assert_equal(0, @machine.get_balance)
-    end
+    assert_raises NotEnoughMoney do @machine.choose(item) end
+    assert_equal(0, @machine.get_balance)
+  end
 
-    def test_choose_an_item_without_enough_money_raises_exception
-      money = 1.00
-      item = "A01"
-      @machine.insert(money)
+  def test_choose_an_item_without_enough_money_raises_exception
+    money = 1.00
+    item = "A01"
+    @machine.insert(money)
 
-      assert_raises NotEnoughMoney do @machine.choose(item) end
-      assert_equal(money, @machine.get_change)
-    end
+    assert_raises NotEnoughMoney do @machine.choose(item) end
+    assert_equal(money, @machine.get_change)
+  end
 
-    def test_choose_an_item_out_of_stock_raises_exception
-      money = 6.00
-      item = 'A04'
+  def test_choose_an_item_out_of_stock_raises_exception
+    money = 6.00
+    item = 'A04'
 
-      excepected_message = "Vending KokoKola"
-      excepected_change = 3.05
+    excepected_message = "Vending KokoKola"
+    excepected_change = 3.05
 
-      @machine.insert(money)
+    @machine.insert(money)
 
-      assert_equal(excepected_message, @machine.choose(item))
-      assert_raises ItemOutOfStock do @machine.choose(item) end
-      assert_equal(excepected_change, @machine.get_change)
-    end
+    assert_equal(excepected_message, @machine.choose(item))
+    assert_raises ItemOutOfStock do @machine.choose(item) end
+    assert_equal(excepected_change, @machine.get_change)
+  end
 
-    def test_choose_an_invalid_item_raises_exception
-      money = 1.00
-      item = 'A05'
+  def test_choose_an_invalid_item_raises_exception
+    money = 1.00
+    item = 'A05'
 
-      @machine.insert(money)
+    @machine.insert(money)
 
-      assert_raises ItemNotFound do @machine.choose(item) end
-      assert_equal(money, @machine.get_change)
-    end
+    assert_raises ItemNotFound do @machine.choose(item) end
+    assert_equal(money, @machine.get_change)
+  end
 
-    def test_that_change_is_equal_to_inserted_money_minus_selected_article_price
-      money = 3.40
-      item = "A01"
+  def test_that_change_is_equal_to_inserted_money_minus_selected_article_price
+    money = 3.40
+    item = "A01"
 
-      excepected_message = "Vending Smarlies"
-      excepected_change = 1.80
+    excepected_message = "Vending Smarlies"
+    excepected_change = 1.80
 
-      @machine.insert(money)
+    @machine.insert(money)
 
-      assert_equal(excepected_message, @machine.choose(item))
-      assert_equal(excepected_change, @machine.get_change)
-    end
+    assert_equal(excepected_message, @machine.choose(item))
+    assert_equal(excepected_change, @machine.get_change)
+  end
 
-    def test_that_change_is_correct_after_selecting_an_item
-      money = 2.10
-      item = "A03"
+  def test_that_change_is_correct_after_selecting_an_item
+    money = 2.10
+    item = "A03"
 
-      excepected_message = "Vending Avril"
-      excepected_change = 0.00
-      excepected_balance = 2.10
+    excepected_message = "Vending Avril"
+    excepected_change = 0.00
+    excepected_balance = 2.10
 
-      @machine.insert(money)
+    @machine.insert(money)
 
-      assert_equal(excepected_message, @machine.choose(item))
-      assert_equal(excepected_change, @machine.get_change)
-      assert_equal(excepected_balance, @machine.get_balance)
-    end
+    assert_equal(excepected_message, @machine.choose(item))
+    assert_equal(excepected_change, @machine.get_change)
+    assert_equal(excepected_balance, @machine.get_balance)
+  end
 
-    def test_that_change_is_correct_after_selecting_multiple_items
-      money = 6.00
-      items = ["A04", "A04", "A01", "A02", "A02"]
+  def test_that_change_is_correct_after_selecting_multiple_items
+    money = 6.00
+    items = ["A04", "A04", "A01", "A02", "A02"]
 
-      excepected_messages = [
-        "Vending KokoKola",
-        "Item KokoKola: Out of stock!",
-        "Vending Smarlies",
-        "Vending Carampar",
-        "Vending Carampar"
-      ]
-      excepected_change = 6.25
-      excepected_balance = 5.75
+    excepected_messages = [
+      "Vending KokoKola",
+      "Item KokoKola: Out of stock!",
+      "Vending Smarlies",
+      "Vending Carampar",
+      "Vending Carampar"
+    ]
+    excepected_change = 6.25
+    excepected_balance = 5.75
 
-      @machine.insert(money)
-      assert_equal(excepected_messages[0], @machine.choose(items[0]))
+    @machine.insert(money)
+    assert_equal(excepected_messages[0], @machine.choose(items[0]))
 
-      @machine.insert(money)
-      assert_raises ItemOutOfStock do @machine.choose(items[1]) end
-      assert_equal(excepected_messages[2], @machine.choose(items[2]))
-      assert_equal(excepected_messages[3], @machine.choose(items[3]))
-      assert_equal(excepected_messages[4], @machine.choose(items[4]))
-      assert_equal(excepected_change, @machine.get_change)
-      assert_equal(excepected_balance, @machine.get_balance)
-    end 
+    @machine.insert(money)
+    assert_raises ItemOutOfStock do @machine.choose(items[1]) end
+    assert_equal(excepected_messages[2], @machine.choose(items[2]))
+    assert_equal(excepected_messages[3], @machine.choose(items[3]))
+    assert_equal(excepected_messages[4], @machine.choose(items[4]))
+    assert_equal(excepected_change, @machine.get_change)
+    assert_equal(excepected_balance, @machine.get_balance)
+  end 
 end
---
+++ tests/models/test_sale.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+
+require_relative '../../models/item'
+require_relative '../../models/sale'
+require_relative '../../models/machine'
+
+class TestSale < MiniTest::Test
+  def setup
+    @machine = Machine.new([
+      Item.new("Smarlies", "A01", 100, 1.60), 
+      Item.new("Carampar", "A02", 50, 0.60), 
+      Item.new("Avril", "A03", 20, 2.10), 
+      Item.new("KokoKola", "A04", 10, 2.95) 
+    ])
+  end
+
+  def test_sales
+    money = 1000.00
+    item = "A01"
+    dates = [ 
+        "2020-01-01T20:30:00",
+        "2020-03-01T23:30:00",
+        "2020-03-04T09:22:00",
+        "2020-04-01T23:00:00",
+        "2020-04-01T23:59:59",
+        "2020-04-04T09:12:00"
+    ]
+
+    excpected_sales = [
+        "Hour 23 generated a revenue of 4.80",
+        "Hour 9 generated a revenue of 3.20",
+        "Hour 20 generated a revenue of 1.60"
+    ]
+
+    @machine.insert(money)
+    dates.each do |date|
+        @machine.set_time(date)
+        @machine.choose(item)
+    end
+
+    best_sales = @machine.best_sales
+    best_sales.each_with_index do |sale, i|
+        assert_equal(excpected_sales[i], "Hour #{sale.first} generated a revenue of #{"%.2f" % [sale.last]}")
+    end
+  end
+end
+
+    
